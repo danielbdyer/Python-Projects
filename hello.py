bae65e46269e138dab7679cf2ced50cd76c3ca5d
Daniel Dyer <danielbdyer@gmail.com>
2017-11-14 13:29:54 -0600

    Updating hello file to be used for assignment

--- hello.py
+++
@@ -1,8 +0,0 @@
-name = raw_input("What is your name? ")
-age = raw_input("What is your age? ")
-
-#message = name + age
-
-message = "My name is " + name + " and my age is " + age + "."
-
-print(message)
