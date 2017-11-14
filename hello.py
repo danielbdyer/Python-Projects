a87c7568a767edd895337abbc9941929ccb15bdd
Daniel Dyer <danielbdyer@gmail.com>
2017-11-14 12:04:03 -0600

    Made some further changes.

---
+++ hello.py
@@ -0,0 +1,8 @@
+name = raw_input("What is your name? ")
+age = raw_input("What is your age? ")
+
+#message = name + age
+
+message = "My name is " + name + " and my age is " + age + "."
+
+print(message)
