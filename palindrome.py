25561f8f4ea788d7d2b9a97c822d99606e80ba8e
Daniel Dyer <danielbdyer@gmail.com>
2017-11-16 15:51:16 -0600

    Optimized the function to direct-compare string and reverse

--- palindrome.py
+++ palindrome.py
@@ -1,18 +1,25 @@
-import math
-
+#import math
 potential_pal = raw_input("What word would you like to check for palindromic nature? ").lower()
 reversed_pal = potential_pal[::-1]
-halfway_mark = int(math.floor(len(potential_pal))/2)+1
 
-def palindrome(potential_pal):
-    palindromic_flag = True
-    for index in range(0,halfway_mark):
-        if potential_pal[index] != reversed_pal[index]:
-            palindromic_flag = False
-        else:
-            palindromic_flag = True
-    if palindromic_flag == False:
-        print "This word is not a palindrome!"
-    else: print "This word is a palindrome."
+def isPalindrome(potential_pal):
+    if potential_pal == reversed_pal: print "Palindrome!"; return True
+    else: print "Not a palindrome."; return False
 
-palindrome(potential_pal)
+isPalindrome(potential_pal)
+
+
+#halfway_mark = int(math.floor(len(potential_pal))/2)+1
+
+#def palindrome(potential_pal):
+#    palindromic_flag = True
+#    for index in range(0,halfway_mark):
+#        if potential_pal[index] != reversed_pal[index]:
+#            palindromic_flag = False
+#        else:
+#            palindromic_flag = True
+#    if palindromic_flag == False:
+#        print "This word is not a palindrome!"
+#    else: print "This word is a palindrome."
+
+#palindrome(potential_pal)
