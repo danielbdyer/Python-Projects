ed510207fa5814e23760d27386682a07f992a253
Daniel Dyer <danielbdyer@gmail.com>
2017-11-27 11:56:43 -0600

    Added factorialization to operandcheck

--- checkoper.py
+++ checkoper.py
@@ -1,7 +1,7 @@
 def operandcheck(prompt):
     operand = raw_input(prompt)
-    while operand not in ('+', '-', '*', '/'):
+    while operand not in ('+', '-', '*', '/', '!'):
         print("That's not a valid operand!")
-        print("Please try again using '+', '-', '*', or '/'.")
+        print("Please try again using '+', '-', '*', '/', '!'.")
         operand = raw_input("What's your operand you'd like to use? ")
     return operand
