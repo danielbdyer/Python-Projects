0e25fbe753543dab458b4e6b2ea2129ce236f23e
Daniel Dyer <danielbdyer@gmail.com>
2017-11-29 13:23:33 -0600

    Moving all calculator instances into a single script

---
+++ main.py
@@ -0,0 +1,5 @@
+from palindrome import Palindrome
+palindrome = Palindrome()
+
+potential_pal = raw_input("What word would you like to check for palindromic nature? ").lower()
+palindrome.isPalindrome(potential_pal)
