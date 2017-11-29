b1edd4a8b4a176144a184fb598e5b58314468e41
Daniel Dyer <danielbdyer@gmail.com>
2017-11-29 13:26:11 -0600

    Class method for checking a palindrome

--- palindrome.py
+++ palindrome.py
@@ -1,24 +1,11 @@
-#import math
-potential_pal = raw_input("What word would you like to check for palindromic nature? ").lower()
+class Palindrome(object):
+    def __init__(self):
+        pass
 
-def isPalindrome(potential_pal):
-    if potential_pal == potential_pal[::-1]: print "Palindrome!"; return True
-    else: print "Not a palindrome."; return False
-
-isPalindrome(potential_pal)
-
-
-#halfway_mark = int(math.floor(len(potential_pal))/2)+1
-
-#def palindrome(potential_pal):
-#    palindromic_flag = True
-#    for index in range(0,halfway_mark):
-#        if potential_pal[index] != reversed_pal[index]:
-#            palindromic_flag = False
-#        else:
-#            palindromic_flag = True
-#    if palindromic_flag == False:
-#        print "This word is not a palindrome!"
-#    else: print "This word is a palindrome."
-
-#palindrome(potential_pal)
+    def isPalindrome(self,palindrome_testword):
+        if palindrome_testword == palindrome_testword[::-1]:
+            print(palindrome_testword + " is the exact same back to front!")
+            return palindrome_testword[::-1]
+        else:
+            print(palindrome_testword + " is not a palindrome, as when it is reversed it reads: " + palindrome_testword[::-1])
+            return palindrome_testword[::-1]
