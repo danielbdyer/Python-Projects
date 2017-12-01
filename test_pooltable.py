0004b723b0c6cd64d30dd4400b5bb056a89f51f2
Daniel Dyer <danielbdyer@gmail.com>
2017-12-01 17:53:41 -0600

    Added several more unit tests

--- test_pooltable.py
+++ test_pooltable.py
@@ -1,38 +1,61 @@
-import unittest
-from pooltable import PoolTable
+import unittest, time
+from pooltable import *
 
 class TestPoolTable(unittest.TestCase):
 
     def setUp(self):
-        self.pooltable = Pooltable()
+        pooltable[0] = PoolTable()
 
-    def test_can_create_pooltable_object(self):
-        self.assertNotEqual(self.pooltable,None)
+    def test_creates_pooltable_array(self):
+        self.assertNotEqual(pooltable,None)
 
-    def test_array_length_as_expected(self):
+    def test_array_length_is_twelve_as_expected(self):
+        self.assertEqual(len(pooltable),12)
 
-    def test_can_add_numbers(self):
-        self.assertEqual(6,self.calculator.add(4,2))
-        print("4 plus 2 equals... RETURNED ANSWER: " + str(self.calculator.add(4,2)))
+    def test_initial_status_unoccupied(self):
+        self.assertEqual(pooltable[0].occupied,"NOT OCCUPIED")
 
-    def test_can_subtract_numbers(self):
-        self.assertEqual(2,self.calculator.sub(4,2))
-        print("4 minus 2 equals... RETURNED ANSWER: " + str(self.calculator.sub(4,2)))
+    def test_table_can_be_given_when_unoccupied(self):
+        pooltable[0].giveOut()
+        self.assertEqual(pooltable[0].occupied,"OCCUPIED")
 
-    def test_can_multiply_numbers(self):
-        self.assertEqual(8,self.calculator.mult(4,2))
-        print("4 times 2 equals... RETURNED ANSWER: " + str(self.calculator.mult(4,2)))
+    def test_table_can_not_be_given_when_occupied(self):
+        pooltable[0].giveOut()
+        self.assertEqual(pooltable[0].occupied,"OCCUPIED")
+        self.occupied_refusal = pooltable[0].giveOut()
+        self.assertTrue(self.occupied_refusal.endswith("is currently occupied."))
 
-    def test_can_divide_numbers(self):
-        self.assertEqual(2,self.calculator.div(4,2))
-        print("4 divided by 2 equals... RETURNED ANSWER: " + str(self.calculator.div(4,2)))
+    def test_start_time_recorded_when_table_opened(self):
+        pooltable[0].giveOut()
+        self.assertNotEqual(pooltable[0].start_time,None)
 
-    def test_can_return_modulo(self):
-        self.assertEqual("2R1",self.calculator.div(5,2))
-        print("5 divided by 2 equals... RETURNED ANSWER: " + str(self.calculator.div(5,2)))
+    def test_timer_tally_was_close_to_a_half_second(self):
+        pooltable[0].giveOut()
+        time.sleep(.5)
+        pooltable[0].gatherTimeElapsed()
+        self.assertTrue(.5 < pooltable[0].seconds < .6)
 
-    def test_can_factorialize_numbers(self):
-        self.assertEqual(self.calculator.fact(4),24)
-        print("4 factorialized equals... RETURNED ANSWER: " + str(self.calculator.fact(4)))
+    def test_table_sets_back_to_unoccupied_when_closed(self):
+        pooltable[0].giveOut()
+        pooltable[0].closeOut()
+        self.assertEqual(pooltable[0].occupied,"NOT OCCUPIED")
+
+    def test_can_return_monetary_amount_given_mins(self):
+        pooltable[0].minutes = 20
+        self.assertEqual(pooltable[0].incurredCost(),"$10.00")
+
+    def test_can_return_in_minutes_when_under_60min(self):
+        pooltable[0].minutes = 59
+        self.assertEqual(pooltable[0].readableTime(),"59min")
+
+    def test_can_return_in_hour_min_when_over_60min(self):
+        pooltable[0].minutes = 61
+        self.assertEqual(pooltable[0].readableTime(),"1hr1min")
+
+    def test_can_return_timedelta(self):
+        pooltable[0].giveOut()
+        time.sleep(.5)
+        pooltable[0].closeOut()
+        self.assertTrue(pooltable[0].time_elapsed,not(None))
 
 unittest.main(verbosity=2)
